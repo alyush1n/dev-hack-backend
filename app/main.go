e3153594137fde0071ebe5cbb8362d400fc11085
3xlerman <lermanworks@gmail.com>
2021-06-04 21:10:28 +0300

    feat: config and gin

--- app/main.go
+++ app/main.go
@@ -1,8 +1,23 @@
 package main
 
-import "dev-hack-backend/app/db"
+import (
+	"dev-hack-backend/app/config"
+	"dev-hack-backend/app/db"
+	"github.com/gin-gonic/gin"
+)
 
 func main() {
+
+	config.Load()
+
 	db.Connect()
+
+	app := gin.Default()
+	gin.SetMode(gin.DebugMode)
+
+	app.NoRoute(func(c *gin.Context) {
+		c.JSON(404, gin.H{"message": "not found"})
+	})
+
 
 }
--- go.mod
+++ go.mod
@@ -2,4 +2,8 @@ module dev-hack-backend
 
 go 1.16
 
-require go.mongodb.org/mongo-driver v1.5.3
+require (
+	github.com/gin-gonic/gin v1.7.2
+	github.com/joho/godotenv v1.3.0
+	go.mongodb.org/mongo-driver v1.5.3
+)
