1cf836752cd6be5f6ee1c69f1903c500ca36586c
3xlerman <lermanworks@gmail.com>
2021-06-04 21:14:30 +0300

    feat: config.go

---
+++ app/config/config.go
@@ -0,0 +1,16 @@
+package config
+
+import (
+	"fmt"
+	"github.com/joho/godotenv"
+)
+
+func Load() {
+
+	err := godotenv.Load()
+	if err != nil {
+		fmt.Println("Error in loading configs: " + err.Error())
+	} else {
+
+	}
+}
