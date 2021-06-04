50c82fabe5ad94ff018623071211c1a9b8a73338
izifizik <izifizik.17@gmail.com>
2021-06-04 19:58:20 +0000

    add

--- app/db/connect.go
+++ app/db/connect.go
@@ -1,5 +1,39 @@
 package db
 
-func Connect() {
+import (
+	"context"
+	"fmt"
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
+	"log"
+	"time"
+)
 
+var client *mongo.Client
+
+func Connect() {
+	var err error
+	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+	defer cancel()
+	client, err = mongo.Connect(ctx, options.Client().ApplyURI("mongodb+srv://fizik:dKUAhJHSxBc3JS38zSxN@cluster0.oeuni.mongodb.net/myFirstDatabase?retryWrites=true&w=majority"))
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	err = client.Connect(context.TODO())
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	err = client.Ping(context.TODO(), nil)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Println("Connected to MongoDB!")
+}
+
+func usersCollection() (collection *mongo.Collection) {
+	collection = client.Database("dev-hack").Collection("users")
+	return collection
 }
--- app/main.go
+++ app/main.go
@@ -1,5 +1,7 @@
 package main
 
-func main() {
+import "db"
 
+func main() {
+	db.Connect()
 }
