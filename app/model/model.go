7d884fc5f3473fb55957207b106cd857a5c7e03f
izifizik <izifizik.17@gmail.com>
2021-06-05 15:50:27 +0000

    fix: model update j(b)son

--- app/model/model.go
+++ app/model/model.go
@@ -5,24 +5,20 @@ import (
 )
 
 type User struct {
-	Id        primitive.ObjectID `bson:"_id"`
-	Username  string             `json:"username" bson:"username" binding:"required"`
-	Password  string             `json:"password" bson:"password" binding:"required"`
-	Clubs     []primitive.ObjectID
-	FirstName string
-	LastName  string
-	Sex       string
-	Points    string
-	Stats
-}
-
-type Stats struct {
+	Id        primitive.ObjectID   `bson:"_id"`
+	Username  string               `json:"username" bson:"username" binding:"required"`
+	Password  string               `json:"password" bson:"password" binding:"required"`
+	Clubs     []primitive.ObjectID `json:"clubs" bson:"clubs"`
+	FirstName string               `json:"first_name" bson:"first_name"`
+	LastName  string               `json:"last_name" bson:"last_name"`
+	Sex       string               `json:"sex" bson:"sex"`
+	Points    string               `json:"points" bson:"points"`
 }
 
 type Event struct {
 	Id         primitive.ObjectID `bson:"_id"`
 	Clubs      []string           `json:"clubs" bson:"clubs"`
-	Type       string             `json:"type"`
+	Type       string             `json:"type" bson:"type"`
 	Name       string             `json:"name" bson:"name"`
 	Location   string             `json:"location" bson:"location"`
 	Date       string             `json:"date" bson:"date"`
@@ -36,9 +32,9 @@ type Attachment struct {
 }
 
 type Club struct {
-	Id              primitive.ObjectID `bson:"_id"`
-	Name            string
-	IncomingEvents  []primitive.ObjectID
-	Logo            Attachment
-	BackgroundImage Attachment
+	Id              primitive.ObjectID   `bson:"_id"`
+	Name            string               `json:"name" bson:"name"`
+	IncomingEvents  []primitive.ObjectID `json:"incoming_events" bson:"incoming_events"`
+	Logo            Attachment           `json:"logo" bson:"logo"`
+	BackgroundImage Attachment           `json:"background_image" bson:"background_image"`
 }
