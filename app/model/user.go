4871671f969ab7b36504481619891a422aa288ae
izifizik <izifizik.17@gmail.com>
2021-06-05 02:38:03 +0000

    fix user.go

--- app/model/user.go
+++ app/model/user.go
@@ -9,11 +9,11 @@ type User struct {
 	Id        primitive.ObjectID `bson:"_id"`
 	Username  string             `json:"username" bson:"username" binding:"required"`
 	Password  string             `json:"password" bson:"password" binding:"required"`
-	Clubs     []string
-	FirstName string
-	LastName  string
-	Sex       string
-	Points    string
+	Clubs     []string           `json:"clubs" bson:"clubs"`
+	FirstName string             `json:"first_name" bson:"first_name"`
+	LastName  string             `json:"last_name" bson:"last_name"`
+	Sex       string             `json:"sex" bson:"sex"`
+	Points    string             `json:"points" bson:"points"`
 	Stats
 }
 
@@ -26,7 +26,7 @@ type Event struct {
 	Location   string             `json:"location" bson:"location" binding:"required"`
 	Date       time.Time          `json:"date" bson:"date"`
 	Attachment `json:"attachment" bson:"attachment"`
-	IsUnique   bool
+	IsUnique   bool `json:"is_unique" bson:"is_unique"`
 }
 
 type Attachment struct {
@@ -36,7 +36,7 @@ type Attachment struct {
 }
 
 type Clubs struct {
-	IncomingEvents  []string
-	Logo            Attachment
-	BackgroundImage Attachment
+	IncomingEvents  []string   `json:"incoming_events" bson:"incoming_events"`
+	Logo            Attachment `json:"logo" bson:"logo"`
+	BackgroundImage Attachment `json:"background_image" bson:"background_image"`
 }
